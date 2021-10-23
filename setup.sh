b768a06ab8a0631e28f29a96b99c5f5a27673309
nobody <23215696+taicaile@users.noreply.github.com>
2021-10-23 09:57:18 +0800

    update

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 # This script is used to setup a local environment for Python3 development
 
-# config direnv
-
 # Check if direnv is installed, try to install direnv if not installed yet
 DIRENV=direnv
 apt -qq list $DIRENV 2>/dev/null | grep -qE "(installed|upgradeable)" || apt -qq install $DIRENV || {
@@ -20,15 +18,21 @@ grep -qF -- "$DIRENV_HOOK_LINE" "$BASHRC" || {
     source $BASHRC
 }
 
-FILES=(".envrc" ".pre-commit-config.yaml" "pyproject.toml")
+FILES=(".envrc" ".pre-commit-config.yaml" "pyproject.toml" ".gitignore")
 for FILE in "${FILES[@]}"; do
     if [[ ! -f "$FILE" ]]; then
         ENVRC_URL=https://raw.githubusercontent.com/taicaile/pyproject/master/$FILE
-        wget -O "$FILE"  "$ENVRC_URL" || {
+        wget -qO "$FILE"  "$ENVRC_URL" || {
             echo "download failed, kindly check your network or the link: $ENVRC_URL"
             exit 1
         }
     fi
 done
 
+# init repo
+if [ -d .git ]; then
+  git init
+fi
+
+# run direnv
 direnv allow
