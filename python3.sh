d911f24d17d0b7d2ab996923b41e25514b604588
Neil <mailto:23215696+taicaile@users.noreply.github.com>
2022-02-04 19:50:35 +0800

    update

--- python3.sh
+++ python3.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/bash
 
 info() {
-  printf '\E[32m'; echo "$@"; printf '\E[0m'
+  printf '\E[32m'
+  echo "$@"
+  printf '\E[0m'
 }
 
 error() {
-  printf '\E[31m'; echo "$@"; printf '\E[0m'
+  printf '\E[31m'
+  echo "$@"
+  printf '\E[0m'
 }
 
 # check installled Python version
 # -------------------------
 info "The following versions of Python are installed in this machine:"
-for p in $(find  /usr/bin/python* | grep -P 'python[\d](.[\d])?+$')
-do
+for p in $(find /usr/bin/python* | grep -P 'python[\d](.[\d])?+$'); do
   info "$p" "$($p --version)"
 done
 
 # install python3-pip, python3-venv, python3-dev
 # ----------------------------------------------
 apt -q install -y --no-install-recommends \
-    python3.8 \
-    python3.8-dev \
-    python3.8-venv
+  python3 \
+  python3-dev \
+  python3-venv \
+  python3-pip
 
 # check installled Python version
 # -------------------------
 info "The following versions of Python are installed in this machine:"
-for p in $(find  /usr/bin/python* | grep -P 'python[\d](.[\d])?+$')
-do
+for p in $(find /usr/bin/python* | grep -P 'python[\d](.[\d])?+$'); do
   info "$p" "$($p --version)"
 done
--- readme.md
+++ readme.md
@@ -17,7 +17,7 @@ To install a virtual environment for a Python project,
 curl -sL https://raw.githubusercontent.com/taicaile/pyproject/master/view.sh | bash
 ```
 
-To install Python3.8,
+To install Python3,
 
 ```bash
 curl -sL https://raw.githubusercontent.com/taicaile/pyproject/master/python3.sh | bash
@@ -25,6 +25,16 @@ curl -sL https://raw.githubusercontent.com/taicaile/pyproject/master/python3.sh 
 
 To install `markdownlint`,
 
+First, install the `npm` lts version via `nvm`,
+
 ```bash
-sudo npm install -g markdownlint-cli
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
+nvm use --lts
+```
+
+Then, install the required package,
+
+```bash
+npm install -g markdownlint-cli
 ```
